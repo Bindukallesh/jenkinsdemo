b31521e983c66fdd84583dd9175d424e896b02bf
Bindukallesh <95361481+Bindukallesh@users.noreply.github.com>
2024-10-14 17:24:04 +0530

    Update App.java

--- src/main/java/com/ust/core/App.java
+++ src/main/java/com/ust/core/App.java
@@ -3,7 +3,7 @@ package com.ust.core;
 /**
  * Hello world!
  */
-@SpringBootApplication
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
